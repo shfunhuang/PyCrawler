3ed63d623974068f1c2891886e95c99b6b3bea43
shfunhuang <shadowshaikh@smail.nchu.edu.tw>
2015-05-30 10:42:45 +0800

    update YAHOO_MOVIES.py

--- YAHOO_MOVIES.py
+++ YAHOO_MOVIES.py
@@ -10,10 +10,12 @@ import sys
 import numpy
 import time
 from BeautifulSoup import BeautifulSoup
-#reload(sys)
-#sys.setdefaultencoding('utf8')
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 f1 = open("./data/Yahoo/movie.txt", "w")
+f2 = open("./data/Yahoo/director.txt", "w")
+f3 = open("./data/Yahoo/performer.txt", "w")
 
 ### 電影id區間 
 start_id = 1
@@ -41,10 +43,27 @@ for m_id in range(start_id, end_id + 1):
         for p in item.findAll('p'):
             dta.append(p.find('span', {'class':'dta'}).text)
         
-        f1.write(item.find('h4').text.encode('utf-8') + ";" + item.find('h5').text.encode('utf-8') + ";" + ';'.join(dta).encode('utf-8') + ";" + vote.text.encode('utf-8') + ";" + vsum.text.encode('utf-8'))
+        #print item.find('h4').text.encode('utf-8') + "," + item.find('h5').text.encode('utf-8') + "," + ','.join(dta).encode('utf-8')
+        #f1.write(item.find('h4').text.encode('utf-8') + ";" + item.find('h5').text.encode('utf-8') + ";" + ','.join(dta).encode('utf-8') + ";" + vote.find("strong").text.encode('utf-8') + ";" + vote.find("q").text.encode('utf-8') + ";" + vsum.find("em").text.encode('utf-8') + ";" + vsum.find("q").text.encode('utf-8'))
+        f1.write(str(m_id) + ";" + item.find('h4').text.encode('utf-8') + ";" + item.find('h5').text.encode('utf-8') + ";" + vote.find("strong").text.encode('utf-8') + ";" + vote.find("q").text.encode('utf-8') + ";" + vsum.find("em").text.encode('utf-8') + ";" + vsum.find("q").text.encode('utf-8'))
         f1.write("\n")
+		        
+        name = []
+        ### 演員過濾中文 & 特殊符號
+        for i in ",".join(dta).split(",")[3:-2]:
+            for j in re.sub(r'[\x80-\xff]{3,}', '', str(i.encode('utf-8'))).split(")("):
+                name.append(re.sub('\(|\)', '', j))
+                #print re.sub('\(|\)', '', j)
+        #print str(m_id) + ";" + name[0]
+        #print str(m_id)  + ";" + ','.join(name[1:])  
+        f2.write(str(m_id) + ";" + name[0])
+        f2.write("\n")
+        f3.write(str(m_id)  + ";" + ','.join(name[1:]))
+        f3.write("\n")
             
     except BaseException, e:
         print e
 		
 f1.close()
+f2.close()
+f3.close()
