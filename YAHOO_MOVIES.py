b18f2cd1e6c568f68d4aba7f6d01f26f31603c89
shfunhuang <shadowshaikh@smail.nchu.edu.tw>
2015-05-31 17:53:42 +0800

    update YAHOO_MOVIES.py

--- YAHOO_MOVIES.py
+++ YAHOO_MOVIES.py
@@ -6,25 +6,23 @@ Created on Sat Apr 25 12:09:25 2015
 """
 
 import requests
-#import sys
 import numpy
 import time
 import re
 from BeautifulSoup import BeautifulSoup
-#reload(sys)
-#sys.setdefaultencoding('utf8')
 
+f0 = open("./data/Yahoo/miss.txt", "w")
 f1 = open("./data/Yahoo/movie.txt", "w")
 f2 = open("./data/Yahoo/director.txt", "w")
 f3 = open("./data/Yahoo/performer.txt", "w")
 
 ### 電影id區間 
+miss = 0
 start_id = 1
-end_id = 20
+end_id = 5808
 
 for m_id in range(start_id, end_id + 1):
     time.sleep(abs(numpy.random.randn()))
-    print m_id
     try:
         url = 'https://tw.movies.yahoo.com/movieinfo_main.html/id=' + str(m_id)
     
@@ -36,35 +34,39 @@ for m_id in range(start_id, end_id + 1):
         vote = soup.find('div', {'class':'vote clearfix'})
         vsum = soup.find('div', {'class':'sum'})
         
-        ### 跳過空白頁面
+        ### 跳過無資料頁面
         if len(item) == 13:
+            miss = miss + 1
+            f0.write(url)
+            f0.write("\n")
             continue
         
         dta = []
         for p in item.findAll('p'):
             dta.append(p.find('span', {'class':'dta'}).text)
         
+        name = []
+        ### 演員過濾中文 & 特殊符號 & 數字
+        for i in ",".join(dta).split(",")[3:-2]:
+            for j in re.sub(r'[\x80-\xff]{3,}|(\d+)', '', str(i.encode('utf-8'))).split(")("):
+                name.append(re.sub('\\n', ',', re.sub('\(|\)|', '', j)))
+                #print re.sub('\(|\)', '', j)
+        #print str(m_id) + ";" + name[0]
+        #print str(m_id)  + ";" + ','.join(name[1:])                  
         #print item.find('h4').text.encode('utf-8') + "," + item.find('h5').text.encode('utf-8') + "," + ','.join(dta).encode('utf-8')
         #f1.write(item.find('h4').text.encode('utf-8') + ";" + item.find('h5').text.encode('utf-8') + ";" + ','.join(dta).encode('utf-8') + ";" + vote.find("strong").text.encode('utf-8') + ";" + vote.find("q").text.encode('utf-8') + ";" + vsum.find("em").text.encode('utf-8') + ";" + vsum.find("q").text.encode('utf-8'))
         f1.write(str(m_id) + ";" + item.find('h4').text.encode('utf-8') + ";" + item.find('h5').text.encode('utf-8') + ";" + vote.find("strong").text.encode('utf-8') + ";" + vote.find("q").text.encode('utf-8') + ";" + vsum.find("em").text.encode('utf-8') + ";" + vsum.find("q").text.encode('utf-8'))
         f1.write("\n")
-
-        ### 導演(f2), 演員(f3)過濾中文 & 特殊符號		        
-        name = []
-        for i in ",".join(dta).split(",")[3:-2]:
-            for j in re.sub(r'[\x80-\xff]{3,}', '', str(i.encode('utf-8'))).split(")("):
-                name.append(re.sub('\(|\)', '', j))
-                #print re.sub('\(|\)', '', j)
-        #print str(m_id) + ";" + name[0]
-        #print str(m_id)  + ";" + ','.join(name[1:])  
         f2.write(str(m_id) + ";" + name[0])
         f2.write("\n")
         f3.write(str(m_id)  + ";" + ','.join(name[1:]))
         f3.write("\n")
-            
+        
     except BaseException, e:
         print e
-		
+
+print "missing:" + str(miss)
+f0.close()
 f1.close()
 f2.close()
 f3.close()
